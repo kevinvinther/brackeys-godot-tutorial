0134b05e03ce6a2be73a8211cb9818fa7bb83bc9
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-02-24 12:33:44 +0100

    feat(dying): add you died label after dying

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,12 +1,29 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var you_died: Label = get_node("/root/Game/Labels/YouDied")
+@onready var camera_2d: Camera2D = get_node("/root/Game/Player/PlayerCamera")
+
+var dead = false
+
+func center_label():
+	var camera_position : Vector2 = camera_2d.get_screen_center_position()
+	var label_size = you_died.get_minimum_size()
+	you_died.set_position((camera_position - (label_size/2)))
+	
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died")
+	dead = true
+	you_died.text = "You Died"
+	center_label()
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
+	
+func _process(delta: float) -> void:
+	if dead:
+		center_label()
 
 
 func _on_timer_timeout() -> void:
